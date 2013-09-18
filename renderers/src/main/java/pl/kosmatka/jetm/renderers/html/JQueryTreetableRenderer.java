b0c019e3a2e2ba07ec432f92bf99facddf89ef23
krzysztofkos <k.kosmatka@atrem.pl>
2013-09-18 11:32:23 +0200

    initial commit

---
+++ renderers/src/main/java/pl/kosmatka/jetm/renderers/html/JQueryTreetableRenderer.java
@@ -0,0 +1,14 @@
+package pl.kosmatka.jetm.renderers.html;
+
+import java.util.Map;
+
+import etm.core.renderer.MeasurementRenderer;
+
+public class JQueryTreetableRenderer implements MeasurementRenderer {
+
+	public void render(Map points) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
