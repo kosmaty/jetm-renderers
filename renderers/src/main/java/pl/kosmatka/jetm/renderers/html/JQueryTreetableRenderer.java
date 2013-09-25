b7196eb47b52d9ba2a5ed07432f8384f8f023078
krzysztofkos <k.kosmatka@atrem.pl>
2013-09-25 14:50:58 +0200

    First working version

--- renderers/src/main/java/pl/kosmatka/jetm/renderers/html/JQueryTreetableRenderer.java
+++ renderers/src/main/java/pl/kosmatka/jetm/renderers/html/JQueryTreetableRenderer.java
@@ -1,13 +1,104 @@
 package pl.kosmatka.jetm.renderers.html;
 
+import java.text.NumberFormat;
+import java.util.Collection;
 import java.util.Map;
 
+import etm.core.aggregation.Aggregate;
 import etm.core.renderer.MeasurementRenderer;
 
 public class JQueryTreetableRenderer implements MeasurementRenderer {
 
+	private String htmlTableContent = "<b>Measurement not rendered</b>";
+
 	public void render(Map points) {
-		// TODO Auto-generated method stub
+		Collection<Aggregate> aggregates = points.values();
+		StringBuilder builder = new StringBuilder();
+		builder.append("<table id='jetm_result'>\n");
+		builder.append("<thead>\n");
+		builder.append("<tr>\n");
+		builder.append("<th>Measurement Point</th>\n");
+		builder.append("<th>#</th>\n");
+		builder.append("<th>Average</th>\n");
+		builder.append("<th>Min</th>\n");
+		builder.append("<th>Max</th>\n");
+		builder.append("<th>Total</th>\n");
+		builder.append("</tr>\n");
+		builder.append("</thead>\n");
+		builder.append("<tbody>\n");
+		int i = 1;
+		for (Aggregate aggregate : aggregates) {
+			appendRowsToStringBuilder(builder, aggregate, null,
+					Integer.toString(i));
+			i++;
+		}
+		builder.append("</tbody>\n");
+		builder.append("</table>\n");
+		htmlTableContent = builder.toString();
+
+	}
+
+	public void appendRowsToStringBuilder(StringBuilder builder,
+			Aggregate aggregate, String parentRowId, String rowId) {
+		builder.append("<tr data-tt-id='");
+		builder.append(rowId);
+		builder.append("'");
+		if (parentRowId != null) {
+			builder.append(" data-tt-parent-id='");
+			builder.append(parentRowId);
+			builder.append("'");
+		}
+		builder.append("><td>");
+		String pattern = "%,.3f";
+		builder.append(aggregate.getName());
+		builder.append("</td>");
+		builder.append("<td>");
+		builder.append(aggregate.getMeasurements());
+		builder.append("</td>");
+		builder.append("<td>");
+		builder.append(String.format(pattern, aggregate.getAverage()));
+		builder.append("</td>");
+		builder.append("<td>");
+		builder.append(String.format(pattern, aggregate.getMin()));
+		builder.append("</td>");
+		builder.append("<td>");
+		builder.append(String.format(pattern, aggregate.getMax()));
+		builder.append("</td>");
+		builder.append("<td>");
+		builder.append(String.format(pattern, aggregate.getTotal()));
+		builder.append("</td>");
+		builder.append("</tr>\n");
+		int i = 1;
+		for (Object child : aggregate.getChilds().values()) {
+			appendRowsToStringBuilder(builder, (Aggregate) child, rowId, rowId
+					+ "-" + i);
+			i++;
+		}
+	}
+
+	public String getHtmlDocument() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("<!DOCTYPE html>\n");
+		builder.append("\n");
+		builder.append("<html>\n");
+		builder.append("<head>\n");
+		builder.append("<meta charset='utf-8'>\n");
+		builder.append("<link rel='stylesheet' href='http://ludo.cubicphuse.nl/jquery-treetable/stylesheets/jquery.treetable.css' />\n");
+		builder.append("<link rel='stylesheet' href='http://ludo.cubicphuse.nl/jquery-treetable/stylesheets/jquery.treetable.theme.default.css' />\n");
+		builder.append("<title>JETM</title>\n");
+		builder.append("</head>\n");
+		builder.append("<body>\n");
+
+		builder.append(htmlTableContent);
+
+		builder.append("<script src='http://code.jquery.com/jquery-1.10.2.min.js'></script>\n");
+		builder.append("<script src='http://code.jquery.com/ui/1.10.3/jquery-ui.min.js'></script>\n");
+		builder.append("<script src='http://ludo.cubicphuse.nl/jquery-treetable/javascripts/src/jquery.treetable.js'></script>\n");
+		builder.append("<script>$('#jetm_result').treetable({expandable: true});</script>\n");
+		builder.append("</body>\n");
+		builder.append("</html>\n");
+
+		return builder.toString();
 
 	}
 
--- renderers/src/test/java/pl/kosmatka/jetm/renderers/AppTest.java
+++ renderers/src/test/java/pl/kosmatka/jetm/renderers/AppTest.java
@@ -1,38 +1,51 @@
 package pl.kosmatka.jetm.renderers;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import pl.kosmatka.jetm.renderers.html.JQueryTreetableRenderer;
+import etm.core.aggregation.Aggregate;
+import etm.core.aggregation.ExecutionAggregate;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+	public void testRenderer() {
+		ExecutionAggregate aggregate1 = new TestAggregate("test 1");
+		ExecutionAggregate aggregate2 = new TestAggregate("test 2");
+		ExecutionAggregate aggregate3 = new TestAggregate("test 3");
+		ExecutionAggregate aggregate1_1 = new TestAggregate("test 1_1");
+		ExecutionAggregate aggregate1_2 = new TestAggregate("test 1_2");
+		ExecutionAggregate aggregate1_1_1 = new TestAggregate("test 1_1_1");
+		ExecutionAggregate aggregate2_1 = new TestAggregate("test 2_1");
+		ExecutionAggregate aggregate3_1 = new TestAggregate("test 3_1");
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		aggregate1.getChilds().put(aggregate1_1.getName(), aggregate1_1);
+		aggregate1.getChilds().put(aggregate1_2.getName(), aggregate1_2);
+		aggregate1_1.getChilds().put(aggregate1_1_1.getName(), aggregate1_1_1);
+		aggregate2.getChilds().put(aggregate2_1.getName(), aggregate2_1);
+		aggregate3.getChilds().put(aggregate3_1.getName(), aggregate3_1);
+
+		Map<String, Aggregate> map = new HashMap<String, Aggregate>();
+		map.put(aggregate1.getName(), aggregate1);
+		map.put(aggregate2.getName(), aggregate2);
+		map.put(aggregate3.getName(), aggregate3);
+
+		JQueryTreetableRenderer renderer = new JQueryTreetableRenderer();
+		renderer.render(map);
+
+		System.out.println(renderer.getHtmlDocument());
+	}
+}
+
+class TestAggregate extends ExecutionAggregate {
+	public TestAggregate(String name) {
+		super(name);
+		setChilds(new HashMap());
+	}
 }
